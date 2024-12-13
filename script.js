2c4584d41e1dbb24c9373b0221a223cb0c9499fb
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-12-14 03:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function getPromise1(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Promise 1", "2");
+		},2000);
+	})
+}
+
+function getPromise2(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Promise 2", "1");
+		},1000);
+	})
+}
+
+function getPromise3(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Promise 3" , "3");
+		},3000);
+	})
+}
+
+const p = Promise.all([getPromise1(),getPromise2(),getPromise3()]);
+
+p.then((values)=>{
+	values.map((value)=>{
+		va
+	})
+})
+
